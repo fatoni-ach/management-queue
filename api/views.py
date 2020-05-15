33aa9faf96f03bdbf5656d1b26030160d3520c99
Ach Fatoni <fatoni.noah@gmail.com>
2020-05-16 01:59:19 +0700

    add api

--- api/views.py
+++ api/views.py
@@ -2,16 +2,24 @@ from django.shortcuts import render, HttpResponse
 from rest_framework.response import Response
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.middleware.csrf import get_token
 
 # Create your views here.
 def index(request):
+    if(request.method == 'GET' ):
+        token = get_token(request)
+        return JsonResponse({'token': token, 'success': 'true'})
+    else:
+        return JsonResponse({'error': 'true', 'msg': 'Invalid secret'})
+    
     if request.method == "POST":
+        print(request.POST)
         if request.POST['action'] == "cek_id":
             context = {
                 'status':'ada',
                 'no_antrian':15,
             }
-            return JsonResponse(context)
+            return HttpResponse(context)
     context = {
         'title':'api'
     }
