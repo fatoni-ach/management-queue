cec7839c9478758404f488c7871f5f0f07a71a0b
Ach Fatoni <fatoni.noah@gmail.com>
2020-05-17 01:48:56 +0700

    api v1.1

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     # url(r'^$', views.index, name="index"),
     url(r'^add/$', views.TambahAntrian.as_view(), name='tambah-antrian'),
+    url(r'^list/$', views.list_pasien, name='list'),
 ]
