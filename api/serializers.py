ced8493eef043c6b4d53636f6a01526a175686de
Ach Fatoni <fatoni.noah@gmail.com>
2020-05-17 01:44:24 +0700

    api v1.1

--- api/serializers.py
+++ api/serializers.py
@@ -16,6 +16,43 @@ class addSerializers(ModelSerializer):
             'gol_darah',
             'no_telp',
             'alamat',
-            'created_at',
-            'updated_at',
+        ]
+
+class pasienSerializers(ModelSerializer):
+    class Meta:
+        model = Pasien
+        fields      = [
+            'nama_pasien',
+            'jenis_kelamin',
+            'umur',
+            'nama_dokter',
+            'jenis_pengobatan',
+            'waktu_mulai',
+            'waktu_berakhir',
+            'durasi_pengobatan',
+        ]
+
+class noAntrianSerializers(ModelSerializer):
+    class Meta:
+        model = NoAntrian
+        fields  = [
+            'no',
+            'durasi',
+            'data_pasien',
+            'status',
+            'pemanggil',
+        ]
+
+class dataPasienSerializers(ModelSerializer):
+    class Meta:
+        model = DataPasien
+        fields = [
+            'nama_pasien',
+            'no_ktp',
+            'jenis_kelamin',
+            'tempat_tgl_lahir',
+            'Status',
+            'gol_darah',
+            'no_telp',
+            'alamat',
         ]
